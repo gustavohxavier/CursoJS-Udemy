99e407685d05ab569dc1d4f038e5271b549020a7
Gustavo Hidalgo Xavier <guhidalgox@gmail.com>
2022-11-09 20:54:41 -0300

    aula06, variáveis

---
+++ aula06/index.js
@@ -0,0 +1,10 @@
+/*let nome = 'João';
+
+console.log(nome, 'nasceu em 1984.');
+console.log('Em 2000', nome, 'conheceu Maria.');*/
+
+//NÃO UTILIZE VAR, UTILIZE LET.
+
+let nome;
+nome = 'Qualquer valor';
+console.log(nome);
